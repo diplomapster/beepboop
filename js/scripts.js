c54afb361d49dc90a2676182904b318197780f4c
Brendan Moroso <brendan.moroso@gmail.com>
2019-04-07 13:46:02 -0700

    Added first spec to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
+//User Interface
 $(document).ready(function() {
   $("#submit").click(function() {
 
-    var number = parseInt($("#number").val());
-    var array =[];
-    $("#response").text(array);
+    // var number = parseInt($("#number").val());
+    var number = $("#number").val();
+    var array = [];
+        for (currentNumber = 0; currentNumber <= number; currentNumber +=1){
+          array.push(currentNumber);
+        }
+        $("#response").append(array + "<br>");
 
-    function beepboop(number){
-      for (i = 0; i <= number; i++){
-      }array.push(i)
-    }
   });
 });
