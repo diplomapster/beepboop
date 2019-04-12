8eabe869618596e8e64dfe7dbc1c8cbc8e3b7900
Brendan Moroso <brendan.moroso@gmail.com>
2019-04-12 14:23:59 -0700

    Remove unnecessary additional function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,30 @@
 //Backend Logic
-//initial empty array
+
+//create initial empty array
 var array = [];
 
 //this function takes a user's number and pushes it and every number less than it into the empty array.
 function populateArray() {
   var number = parseInt($("#number").val());
-  // var number = toString($("#number").val());
-  for (currentNumber = 0; currentNumber <= number; currentNumber +=1){
-    array.push(currentNumber);
+  for (var currentNumber = 0; currentNumber <= number; currentNumber +=1){
+    // currentNumber.split("").map(function(currentNumber){
+      if (currentNumber === 3){
+        array.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      } else if (currentNumber === 2) {
+        array.push("Boop");
+      } else if (currentNumber === 1){
+        array.push("Beep");
+      } else {
+        array.push(currentNumber);
+      }
+    }
   }
-}
 
-function changeNumbers(){
-  if (3 === true){
-    replace(3, "I'm sorry, Dave. I'm afraid I can't do that.");
-  }
-}
 
 //User Interface
 $(document).ready(function() {
   $("#submit").click(function() {
     populateArray();
-    array.forEach(changeNumbers);
-
-    $("#response").append(array + "<br>");
+    $("#response").text(array);
   });
 });
