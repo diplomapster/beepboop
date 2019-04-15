ba96f7f3c07fc323c59e481c68adceb622032317
Brendan Moroso <brendan.moroso@gmail.com>
2019-04-15 15:04:21 -0700

    Added functionality for numbers greater than 9.

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,14 @@
 var array = [];
 
 //this function takes a user's number and pushes it and every number less than it into the empty array.
-function populateArray() {
-  var number = parseInt($("#number").val());
-  for (var currentNumber = 0; currentNumber <= number; currentNumber +=1){
-    // currentNumber.split("").map(function(currentNumber){
-      if (currentNumber === 3){
+function populateArray(number) {
+  for (var i = 0; i <= number; i +=1){
+    var currentNumber = i.toString();
+      if (currentNumber.includes(3)){
         array.push("I'm sorry, Dave. I'm afraid I can't do that.")
-      } else if (currentNumber === 2) {
+      } else if (currentNumber.includes(2)) {
         array.push("Boop");
-      } else if (currentNumber === 1){
+      } else if (currentNumber.includes(1)){
         array.push("Beep");
       } else {
         array.push(currentNumber);
@@ -23,7 +22,8 @@ function populateArray() {
 //User Interface
 $(document).ready(function() {
   $("#submit").click(function() {
-    populateArray();
+    var number = $("#number").val();
+    populateArray(number);
     $("#response").text(array);
   });
 });
